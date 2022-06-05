3cbb4b439f963266cac7222d4b8e1070523764f2
Craig Boardman <craig_boardman@trimble.com>
2022-06-06 11:10:27 +1200

    Remove dependency on lcdproc-client - just use directly

--- src/server/lcdproc.ts
+++ src/server/lcdproc.ts
@@ -1,49 +1,83 @@
-import * as config from '../config/config.json';
+import config from '../config/config.json';
 import { TiltPayload } from '../common/tiltpayload';
+import { Socket } from 'net';
 
 class LcdProc {
-  private lc: any;
+  private socket: Socket;
+  private host = 'localhost';
+  private port = 13666;
+  private width = 0;
+  private height = 0;
+  private connected = false;
 
-  constructor() {
-    const LcdClient = require('lcdproc-client').LcdClient;
+  constructor(host: string, port = 13666) {
+    this.host = host;
+    this.port = port;
 
-    try {
-      const lc = new LcdClient(13666, config.server.LCDd ?? 'localhost');
+    this.socket = new Socket();
+    this.socket.connect(this.port, this.host, () => {
+      this.socket.write('hello\n');
+      this.connected = true;
+    });
 
-      lc.on('init', function () {
-        console.log('LCD Proc initialized...');
-      });
-      lc.on('ready', function () {
-        console.log(`LCDProc: width=${lc.width}, height=${lc.height}`);
-        lc.screen('brew-bot');
-        lc.widget('Temperature');
-        lc.widget_val('Temperature', 1, 1, '???');
-        lc.widget('Gravity');
-        lc.widget_val('Gravity', 1, 2, '???');
-      });
-      lc.init();
-      this.lc = lc;
-    } catch (error) {
-      console.log(`Failed to connect to LCDd @ ${config.server.LCDd}`);
-    }
+    this.socket.on('data', (d) => {
+      const data_str = d.toString();
+      const [cmd, ...params] = data_str.split(' ');
+      if (cmd == 'connect') {
+        const pmap = params.reduce((acc: { [k: string]: string }, v, i, a) => {
+          if (i % 2) acc[a[i - 1]] = v;
+          return acc;
+        }, {});
+        this.width = parseInt(pmap['wid'] ?? '0');
+        this.height = parseInt(pmap['hgt'] ?? '0');
+
+        this.socket.write('client_set name {beer-bot}\n');
+        console.log(`LCDProc: width=${this.width}, height=${this.height}`);
+        this.screen();
+      }
+    });
+
+    this.socket.on('error', (error) => {
+      console.log(`lcdproc: socket error (${error})`);
+      this.connected = false;
+    });
+  }
+
+  screen() {
+    this.socket.write('screen_add screen\n');
+    this.socket.write('screen_set screen name {screen}\n');
+    this.socket.write('screen_set screen heartbeat on\n');
+    this.socket.write('screen_set screen priority 2\n');
+    this.socket.write('screen_set screen backlight on\n');
+    this.socket.write(this.widget('Temperature'));
+    this.socket.write(this.widget_val('Temperature', 1, 1, '???'));
+    this.socket.write(this.widget('Gravity'));
+    this.socket.write(this.widget_val('Gravity', 1, 2, '???'));
+  }
+
+  widget(name: string) {
+    return `widget_add screen ${name} string\n`;
+  }
+
+  widget_val(name: string, x: number, y: number, value: string) {
+    return `widget_set screen ${name} ${x} ${y} {${value}}\n`;
+  }
+
+  temperature_val(temp: number) {
+    return this.widget_val('Temperature', 1, 1, temp.toFixed(1));
+  }
+
+  gravity_val(g: number) {
+    return this.widget_val('gravity', 1, 2, g.toFixed(3));
   }
 
   onPayload(payload: TiltPayload) {
-    if (this.lc) {
-      this.lc.widget_val(
-        'Temperature',
-        1,
-        1,
-        `Temp: ${payload.temperature.toFixed(1)}`
-      );
-      this.lc.widget_val(
-        'Gravity',
-        1,
-        2,
-        `Gravity: ${payload.gravity.toFixed(3)}`
-      );
+    if (!this.connected) {
+      return;
     }
+    this.socket.write(this.temperature_val(payload.temperature));
+    this.socket.write(this.gravity_val(payload.gravity));
   }
 }
 
-export const lcdproc = new LcdProc();
+export const lcdproc = new LcdProc(config.server.LCDd ?? 'localhost');
